4c5df2d4612b9f95f93fb511404ff0bb1802fe9d
BayronFlores <floresbayron133@gmail.com>
2022-11-21 13:23:36 -0300

    a

--- mak4.0/componentes/header.php
+++ mak4.0/componentes/header.php
@@ -16,34 +16,41 @@
             $total_cart_counts = $count_cart_items->rowCount();
          ?>
       <div id="menu-btn" class="container">
-         <a title="home" href="./home.php"><img src="./IMG/home.png" alt="home" width="49" height="48" /></a>
-         <a href="cart.php"><img src="./IMG/carrito.png" alt="carrito" width="49" height="48" /><i class="fas fa-shopping-cart"></i><span>(<?= $total_cart_counts; ?>)</span></a>
-
-      
-
-      <div class="profile" id="img"><a title="usuario" href="#"><img src="./IMG/usuario.png" alt="carrito" width="49" height="48" /></a>
-         <?php          
-            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
-            $select_profile->execute([$user_id]);
-            if($select_profile->rowCount() > 0){
-            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
-         ?>
-         <p><?= $fetch_profile["email"]; ?></p>
 
 
-         <a href="componentes/user_logout.php" class="delete-btn" onclick="return confirm('quiere cerrar su sesion?');">cerrar sesion</a> 
-         <?php
-            }else{
-         ?>
-         <p>porfavor registrese primero</p>
-         <div class="flex-btn">
-            <a href="registrar.php" class="option-btn">registrar</a>
-            <a href="login.php" class="option-btn">login</a>
-         </div>
+      <div class="profile" id="img">
+
+
+      <div class="dropdown">
+                <button class="dropbtn"><img src="./img/usuario.png" alt="usuario" width="49" height="48" /></button>
+                 <div class="dropdown-content">
+                 <?php          
+               $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+               $select_profile->execute([$user_id]);
+               if($select_profile->rowCount() > 0){
+               $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+            ?>
+            <p><?= $fetch_profile["email"]; ?></p>
+   
+   
+            <a href="componentes/user_logout.php" class="delete-btn" onclick="return confirm('quiere cerrar su sesion?');">cerrar sesion</a> 
+            <?php
+               }else{
+            ?>
+            <p>porfavor registrese primero</p>
+            <div class="flex-btn">
+               <a href="registrar.php" class="option-btn">registrar</a>
+               <a href="login.php" class="option-btn">login</a>
+            </div>
+
          <?php
             }
-         ?>      
+         ?>  
+      </div> 
       </div>
+      <a title="home" href="./home.php"><img src="./IMG/home.png" alt="home" width="49" height="48" /></a>
+         <a href="cart.php"><img src="./IMG/carrito.png" alt="carrito" width="49" height="48" /><i class="fas fa-shopping-cart"></i><span>(<?= $total_cart_counts; ?>)</span></a>     
+ 
       </div>
    </section>
 
@@ -89,3 +96,5 @@
                 <a title="caarrito" href="./pedidos/cart.php"><img src="./IMG/carrito.png" alt="carrito" width="49" height="48" /></a>
             </div>
         </div> -->
+
+
