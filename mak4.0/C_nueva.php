2520ad42abcb01de6861c3517727e089e4ddd7eb
BayronFlores <floresbayron133@gmail.com>
2022-11-23 13:04:05 -0300

    cambios

---
+++ mak4.0/C_nueva.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar contraseña</title>
+    <link rel="stylesheet" href="./style/general.css">
+    <link rel="stylesheet" href="./style/C_nueva.css">
+    <link rel="icon" type="image/png" href="./IMG/logo_mak.png" />
+</head>
+<body>
+
+     <div class="recovery">
+        <h1>Recuperar contraseña</h1>
+        <br>
+        <br>
+        <h3>Ingrese contraseña nueva:</h3>
+        <input class="item" type="text" name="contraseñan" placeholder="Contraseña nueva"/>
+        <br>
+        <br>
+        <h3>Confirmar contraseña nueva:</h3>
+        <br>
+        <input class="item" type="text" name="confcontraseña" placeholder="Confirmar contraseña nueva"/>
+        <br>
+        <button type="submit" name="Change_pass">Cambiar</button>
+     </div>
+    
+</body>
+</html>
